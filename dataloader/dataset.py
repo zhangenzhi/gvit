3d13472ee75ada80c0d3f4854801fd78d3526cd5
zhangenzhi <zhangenzhi657@gmail.com>
2023-11-07 14:27:11 +0900

    vit training.

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -4,15 +4,16 @@ from torch.utils.data import Dataset
 class RandomDataset(Dataset):
     def __init__(
         self, 
-        size:  tuple = (5000, 32,32,3), #[num,w,h,c]
+        size:  tuple = (5000,32,32,3), #[num,w,h,c]
+        num_class: int = 10,
         ):
         
         self.size = size
         self.raw_x = np.random.rand(*size)
         self.raw_x = np.float32(self.raw_x)
         
-        self.raw_y = np.random.randint(0, 9, size[0])
-        self.raw_y = np.identity(10)[self.raw_y]
+        self.raw_y = np.random.randint(0, num_class, size[0])
+        self.raw_y = np.identity(num_class)[self.raw_y]
         self.raw_y = np.float32(self.raw_y)
     
     def __len__(self):
--- train/trainer.py
+++ train/trainer.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 
 from dataloader.dataset import RandomDataset
 from model.mlp_block import MLP
+from model.vision_transformer import VisionTransformer
 
 class Trainer():
     def __init__(self, 
@@ -14,11 +15,13 @@ class Trainer():
                  model_name: str = 'mlp',
                  learning_rate= 1e-4) -> None:
         
+        self.TASK = task_type
         self.BATCH_SIZE = batch_size
         self.EPOCH = epoch
         self.NUM_WORKER = num_worker
         self.SHUFFLE = shuffle
         self.LR = learning_rate
+        self.MODEL = model_name
         
         self.model = self._build_model()
         self.dataloader = self._build_dataloader()
@@ -26,7 +29,10 @@ class Trainer():
         self.loss_fn = self._build_loss()
     
     def _build_dataloader(self):
-        dataset = RandomDataset(size=(5000,32,32,3))
+        if self.TASK == "random":
+            dataset = RandomDataset(size=(5000,32,32,3),num_class=10)
+        elif self.TASK == "imagenet":
+            dataset = RandomDataset(size=(5000,3,256,256), num_class=1000)
         dataloader = DataLoader(dataset=dataset,
                                 batch_size=self.BATCH_SIZE,
                                 shuffle=self.SHUFFLE,
@@ -34,7 +40,17 @@ class Trainer():
         return dataloader
     
     def _build_model(self):
-        model = MLP(in_channels=3072, hidden_channels=[128,64,32,10])
+        if self.MODEL == 'mlp':
+            model = MLP(in_channels=3072, hidden_channels=[128,64,32,10])
+        elif self.MODEL == "vit":
+            model = VisionTransformer(
+                            image_size=256,
+                            patch_size=32,
+                            num_layers=3,
+                            num_heads=4,
+                            hidden_dim=128,
+                            mlp_dim=128,
+                            num_classes=1000)
         return model
     
     def _build_optimizer(self):
@@ -45,17 +61,27 @@ class Trainer():
         loss_fn = torch.nn.CrossEntropyLoss()
         return loss_fn
     
-    def train_step(self, x, label):
-        x = torch.flatten(x, start_dim=1)
-        y_pred = self.model(x)
-        loss = self.loss_fn(y_pred, label)
-        loss.backward()
-        self.opt.step()
-        self.opt.zero_grad()
-        return loss
+    # def train_step(self, x, label, correct):
+    #     # x = torch.flatten(x, start_dim=1)
+    #     y_pred = self.model(x)
+    #     loss = self.loss_fn(y_pred, label)
+    #     loss.backward()
+    #     self.opt.step()
+    #     correct += (y_pred == label).float().sum()
+    #     self.opt.zero_grad()
+    #     return loss, correct
     
     def train(self):
         for e in range(self.EPOCH):
+            correct = 0
             for (i,data) in enumerate(self.dataloader):
-                loss = self.train_step(data[0], data[1])
-            print("Epoch {}, train loss: {}".format(e, loss))
+                x,label = data[0], data[1]
+                y_pred = self.model(x)
+                loss = self.loss_fn(y_pred, label)
+                loss.backward()
+                self.opt.step()
+                correct += (y_pred == label).float().sum()
+                self.opt.zero_grad()
+            accuracy = 100 * correct / len(self.dataloader)
+            print("Epoch {}, train loss: {}, train acc: {}".format(e, loss, accuracy))
+            print(f"Epoch {e}, train loss: {loss}, train acc: {accuracy}")
