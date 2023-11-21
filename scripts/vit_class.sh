40d0b7092f618ed940b6608f3094905d6c3f20d0
zhangenzhi <zhangenzhi657@gmail.com>
2023-11-21 22:36:45 +0900

    test on dgx.

--- scripts/vit_class.sh
+++ scripts/vit_class.sh
@@ -1,16 +1,14 @@
 # export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
 
 
-export RANK=$SLURM_PROCID
-export LOCAL_RANK=$SLURM_LOCALID
-export WORLD_SIZE=$SLURM_LOCALID
-export MASTER_ADDR=$HOSTNAME
-export MASTER_PORT=29500 # default from torch launcher
-export BATCH_DDP=1
+# export RANK=$SLURM_PROCID
+# export LOCAL_RANK=$SLURM_LOCALID
+# export WORLD_SIZE=$SLURM_LOCALID
+# export MASTER_ADDR=$HOSTNAME
+# export MASTER_PORT=29500 # default from torch launcher
+# export BATCH_DDP=1
 
 
-torchrun --nnodes=1 --nproc_per_node=8 main.py
-=======
-export VALUE=8
+export VALUE=1
 
 OMP_NUM_THREADS=$VALUE torchrun --nnodes=1 --nproc_per_node=8  ./main.py
