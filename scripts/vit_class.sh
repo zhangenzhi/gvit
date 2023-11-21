379aa06773bfcd99d5c3d270391be6ec9f313865
zhangenzhi <zhangenzhi657@gmail.com>
2023-11-21 15:10:00 +0900

    test on frontier.

--- scripts/vit_class.sh
+++ scripts/vit_class.sh
@@ -1,3 +1,3 @@
-export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
+# export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
 
-torchrun --nnodes=2 --nproc_per_node=8 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:29400 elastic_ddp.py
+torchrun --nnodes=1 --nproc_per_node=8 main.py
