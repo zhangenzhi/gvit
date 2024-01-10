5e2d0cbc76738d05ca8fbdb6f7079775e3f94d08
enzhi zhang <zhangenzhi657@gmail.com>
2024-01-10 07:56:09 +0000

    feat. add serialized patchify system.

--- test_frontier.py
+++ test_frontier.py
@@ -5,7 +5,7 @@ from torchvision import transforms
 
 print(torch.cuda.get_device_name())
 
-datapath = "/lustre/orion/gen006/proj-shared/enzhi/"
+datapath = "/lustre/orion/bif146/proj-shared/enzhi/"
 
 train_transform = transforms.Compose([
         transforms.Resize((256,256)),
@@ -28,3 +28,5 @@ val_loader = torch.utils.data.DataLoader(imagenet_val,
                                     num_workers=64)
 train_dataset = iter(train_loader)
 val_dataset = iter(val_loader)
+
+print("train samples:{}, val_samples:{}".format(len(imagenet_data), len(imagenet_val)))
