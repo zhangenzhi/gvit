cc741d1a7de0d7060ccf915c3b70400ddb76ca67
zhangenzhi <zhangenzhi657@gmail.com>
2024-03-07 17:13:18 +0900

    fixed upsampling.

--- model/vit_unet.py
+++ model/vit_unet.py
@@ -150,7 +150,8 @@ class ViTUNet(nn.Module):
     def forward(self, x):
         x, x1, x2, x3 = self.encoder(x)
         x = self.decoder(x, x1, x2, x3)
-        x = F.interpolate(x, size=(self.img_dim, self.img_dim), mode='bilinear', align_corners=False)
+        x = nn.Upsample(size=(self.img_dim, self.img_dim), mode='bilinear', align_corners=True)(x)
+        # x = F.interpolate(x, size=(self.img_dim, self.img_dim), mode='bilinear', align_corners=False)
         return x
 
 
