84ee92fbabfea5f2cc8268cfadad3229df1778da
zhangenzhi <zhangenzhi657@gmail.com>
2024-03-14 14:00:38 +0900

    unetr2d summary.

--- model/unetr2d.py
+++ model/unetr2d.py
@@ -307,7 +307,7 @@ if __name__ == "__main__":
                   num_heads=12, 
                   dropout=0.1)
     # print(unetr(torch.randn(1, 3, 1024, 1024)).shape)
-    
+    unetr.cuda()
     from calflops import calculate_flops
     batch_size = 1
     input_shape = (batch_size, 3, 1024, 1024)
