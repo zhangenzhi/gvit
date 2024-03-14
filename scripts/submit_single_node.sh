8816863eb2b00b1cdd14e6ce87d29f090e0c276c
zhangenzhi <zhangenzhi657@gmail.com>
2024-03-14 13:26:40 +0900

    unetr2d summary.

--- scripts/submit_single_node.sh
+++ scripts/submit_single_node.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 #SBATCH -A bif146
-#SBATCH -J Imagenet-ViT
-#SBATCH -o Imagenet-ViT.o%J
-#SBATCH -t 02:00:00
+#SBATCH -o vitunetr2d.o%J
+#SBATCH -t 2:00:00
 #SBATCH -N 1
 #SBATCH -p batch
 
-#export PATH=$PWD/conda/bin:$PATH
-#eval "$($PWD/conda/bin/conda shell.bash hook)"
-set +x
-source ~/miniconda_frontier/etc/profile.d/conda.sh
-conda activate /ccs/home/enzhi/miniconda_frontier/envs/pth
+# export PATH=$PWD/conda/bin:$PATH
+# eval "$($PWD/conda/bin/conda shell.bash hook)"
 
-export LD_PRELOAD="/usr/lib64/libcrypto.so /usr/lib64/libssh.so.4 /usr/lib64/libssl.so.1.1"
+export MIOPEN_DISABLE_CACHE=1 
+export MIOPEN_CUSTOM_CACHE_DIR='pwd' 
+export HOME="/tmp/srun"
+
+# set +x
+# source ~/miniconda_frontier/etc/profile.d/conda.sh
+# conda activate /ccs/home/enzhi/miniconda_frontier/envs/gvit
+
+# export LD_PRELOAD="/usr/lib64/libcrypto.so /usr/lib64/libssh.so.4 /usr/lib64/libssl.so.1.1"
 
 module load PrgEnv-gnu
-module load gcc/11.2.0
-module load rocm/5.4.0
+module load gcc/12.2.0
+module load rocm/5.7.0
 
 # grab nodecount
-nodes=($(cat ${LSB_DJOB_HOSTFILE} | sort | uniq | grep -v login | grep -v batch))
-nnodes=${#nodes[@]}
+# nodes=($(cat ${LSB_DJOB_HOSTFILE} | sort | uniq | grep -v login | grep -v batch))
+# nnodes=${#nodes[@]}
 
-srun python -u test_frontier.py
+# exec
+# srun -n 1 --ntasks-per-node=1 -c 1 python3 vitunetr2d_train.py \
+#         --datapath=./dataset/paip/output_images_and_masks \
+#         --resolution=512 \
+#         --epoch=1000 \
+#         --batch_size=4 \
+#         --savefile=./vis_vitunet_lr
 
-# srun -n 4 --ntasks-per-node=4 -c 7 python -u cls_train.py
-
+srun -n 1 --ntasks-per-node=1 -c 1 python3 ./model/unetr2d.py
